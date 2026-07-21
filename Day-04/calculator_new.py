06a0c044f375023b55bbe0f833f39dda183422d9
pm-rahul <137659215+SujanVijay2392@users.noreply.github.com>
2026-07-21 00:04:42 +0000

    adding files

---
+++ Day-04/calculator_new.py
@@ -0,0 +1,25 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def sub():
+    s = num1 - num2
+    print(s)
+
+def mul():
+    m = num1 * num2
+    print(m)
+
+def div():
+    d = num1 / num2
+    print("value of division is - " + str(d))
+
+
+addition()
+sub()
+mul()
+div()
+
---
+++ Day-04/calculator_new_2.py
@@ -0,0 +1,25 @@
+num1 = 10
+num2 = 5
+
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def sub(num1,num2):
+    s = num1 - num2
+    return s
+
+def mul():
+    m = num1 * num2
+    print(m)
+
+def div():
+    d = num1 / num2
+    print("value of division is - " + str(d))
+
+
+print(addition(5,15))
+print(sub(20,10))
+mul()
+div()
+
